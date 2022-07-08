ca253773b2bc3745309b803f9aef0f6832012e4e
Linjey-git <lingey.ubi@gmail.com>
2022-07-08 21:37:28 +0300

    Initial commit

---
+++ src/main/java/net/seho/forgotten_world/ForgottenWorldMod.java
@@ -0,0 +1,15 @@
+package net.seho.forgotten_world;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ForgottenWorldMod implements ModInitializer {
+	public static final String MOD_ID = "forgotten_world";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
