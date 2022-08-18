b140d72f32a51bae65e83e15c07f6ba94e9daa26
seho <linjey.ubi@gmail.com>
2022-08-18 18:10:56 +0300

    Linux test run

--- src/main/java/net/seho/forgotten_world/ForgottenWorldMod.java
+++ src/main/java/net/seho/forgotten_world/ForgottenWorldMod.java
@@ -1,15 +1,21 @@
 package net.seho.forgotten_world;
 
+import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ForgottenWorldMod implements ModInitializer {
+public class ForgottenWorldMod implements ModInitializer, ClientModInitializer {
 	public static final String MOD_ID = "forgotten_world";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
+
+	}
+
+	@Override
+	public void onInitializeClient() {
 
 	}
 }
